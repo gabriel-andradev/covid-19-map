63c5b35d7c92f288e8eb53bb67a5849827c0c7db
gabriel-andradev <gandrade.dev@gmail.com>
2022-05-06 15:56:31 -0300

    map covid

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,72 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+import PeruFlag from '../../assets/images/peru.png'
+import WorldFlag from '../../assets/images/world.png'
+import PortugalFlag from '../../assets/images/portugal.png'
+import MexicoFlag from '../../assets/images/mexico.png'
+import IndiaFlag from '../../assets/images/india.png'
+import RussiaFlag from '../../assets/images/russia.png'
+
+
+const COUNTRIES = [
+  {
+    label: 'World',
+    value: 'world',
+    flag: WorldFlag
+  },
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'Índia',
+    value: 'india',
+    flag: IndiaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  },
+  {
+    label: 'México',
+    value: 'mexico',
+    flag: MexicoFlag
+  },
+  {
+    label: 'Peru',
+    value: 'peru',
+    flag: PeruFlag
+  },
+  {
+    label: 'Portugal',
+    value: 'portugal',
+    flag: PortugalFlag
+  },
+  {
+    label: 'Rússia',
+    value: 'russia',
+    flag: RussiaFlag
+  }
+]
+
+export default COUNTRIES
